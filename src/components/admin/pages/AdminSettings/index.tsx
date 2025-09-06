375b7cd0c0bbac1256f9eafa989604c50728ba49
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-09-06 16:17:34 -0300

    atualização de bg do label gray-100

--- src/components/admin/pages/AdminSettings/index.tsx
+++ src/components/admin/pages/AdminSettings/index.tsx
@@ -29,24 +29,24 @@ function AdminSettings() {
                     <div className="relative mt-3">
                         <UserCircle className="absolute top-1/2 -translate-y-1/2 left-3 text-gray-400" size={20} />
                         <input id="userAdmin" type="text" step="0.01" className="block pl-10 pr-3 pb-2.5 pt-4 w-full text-sm text-grgray-100 rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                        <label htmlFor="userAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Usuário</label>
+                        <label htmlFor="userAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Usuário</label>
                     </div>
                     <div className="relative mt-3">
                         <Lock className="absolute top-1/2 -translate-y-1/2 left-3 text-gray-400" size={20} />
                         <input id="passwordAdmin" type="password" step="0.01" className="block pl-10 pr-3 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                        <label htmlFor="passwordAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Senha</label>
+                        <label htmlFor="passwordAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Senha</label>
                     </div>
 
                     <div className="relative mt-3">
                         <Lock className="absolute top-1/2 -translate-y-1/2 left-3 text-gray-400" size={20} />
                         <input id="confirmPasswordAdmin" type="password" step="0.01" className="block pl-10 pr-3 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                        <label htmlFor="confirmPasswordAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Confirmar Senha</label>
+                        <label htmlFor="confirmPasswordAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0]gray-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Confirmar Senha</label>
                     </div>
 
                     <div className="relative mt-3">
                         <Phone className="absolute top-1/2 -translate-y-1/2 left-3 text-gray-400" size={20} />
                         <input id="contactAdmin" type="text" step="0.01" className="block pl-10 pr-3 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label htmlFor="contactAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Contato</label>
+                        <label htmlFor="contactAdmin" className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">Contato</label>
                     </div>
 
                     <div className="lg:flex items-center justify-end gap-3 pt-6 mt-6 border-t border-gray-200">
